889bb067fe1bc5e81b4020694eafbb89571b9e38
Eva Gleitz <eva_gleitz@m013393.aw.org>
2018-10-17 13:54:29 -0700

    okay

--- BlackJack.java
+++ BlackJack.java
@@ -1,8 +1,37 @@
-//due on October 31
+//due sometime
 import java.util.*;
 
 public class Blackjack {
 
   Deck d;
-  
+  ArrayList<card> userHand;
+  ArrayList<card> compHand;
+
+  public Blackjack() {
+    d = new Deck();
+    userHand = new ArrayList<card>();
+    compHand = new ArrayList<card>();
+
+  }
+
+  public Integer calculateHand(ArrayList<card> h) {
+
+    return null;
+
+  }
+
+  public static void main(String[] args) {
+
+    Blackjack b= new Blackjack();
+
+    for (int i = 0; i < 10; i++) {
+      Card c = b.d.drawCard();
+      userHand.add(c);
+    }
+
+    for (int i = 0; i < userHand.size(); i++) {
+      System.out.println(userHand.get(i).getName());
+    }
+
+  }
 }
