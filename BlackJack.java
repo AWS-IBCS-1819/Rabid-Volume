42bd03b3d20a1fe826e073bfc58e7b1c0d56158d
Eva Gleitz <eva_gleitz@m013393.aw.org>
2018-10-22 14:42:13 -0700

    Work in Class

--- BlackJack.java
+++ BlackJack.java
@@ -29,12 +29,21 @@ public class Blackjack {
     return total;
   }
 
+  public FIX THIS LATER cardName(ArrayList<Card> h){
+    String name;
+    for (int i = 0; i < h.size(); i++) {
+      name = h.get(i).getName();
+    }
+    return name;
+  }
+  //could make a method that print card names given a hand
+
   public static void main(String[] args) {
 
     Blackjack b = new Blackjack();
     //intial card draw
     //userHand
-    b.d.reset();
+    b.d.reset(); //make sure to reset before shuffle deck
     b.d.shuffle();
     System.out.println("Here is your hand:");
     for (int i = 0; i < 2; i++) {
@@ -43,7 +52,7 @@ public class Blackjack {
     }
 
     for (int i = 0; i < b.userHand.size(); i++) {
-      System.out.println(b.userHand.get(i).getName());
+      System.out.println(b.cardName(userHand));
     }
 
     int user_hand_total = b.calculateHand(b.userHand);
@@ -62,6 +71,7 @@ public class Blackjack {
     boolean runProgram = true;
     Scanner reader = new Scanner(System.in);  // Reading from System.in
 
+    //while loop is for main 1.2.3
     while (runProgram) {
 
     System.out.println("What would you like to do?\n 1. Hit (Draw Card)\n 2. Stay\n 3. Quit\n");
@@ -76,16 +86,17 @@ public class Blackjack {
       System.out.println("New value is " + user_hand_total);
       //System.out.println("You hit 1"); //then go back to drawCard
     }
-    else if (n == 2) {
+    else if (n == 2) { //if we stay then dealer logic starts
       System.out.println("Your oppnent's hand is:\n");
       for (int i = 0; i < b.compHand.size(); i++) {
         System.out.println(b.compHand.get(i).getName());
       }
       System.out.println("Value is " + comp_hand_total);
+      break;
       //System.out.println("you hit 2"); //then game progresses
     }
     else if (n == 3) {
-      System.out.println("Cheers :)"); //program ends
+      System.out.println("Cheers :)"); //program ends completly
       System.exit(0);
     }
     else {
@@ -93,9 +104,26 @@ public class Blackjack {
     }
 
   }
-
   reader.close();
 
-//make sure to reset before shuffle deck
+  //while loop for what happens after (compHand)
+  //dealer logic if <= 16 drawCard() else stay
+  //dealer logic is another while loop if over 16 or value > 21 then break loop
+  if (comp_hand_total <= 16) { //MAKE A METHOD FOR THIS BECAUSE IT IS DOES NOT GO FOR MY AESTHETIC
+    b.compHand.add(b.d.drawCard());
+    System.out.println("Dealer drew a " + b.compHand.get(b.userHand.size()-1).getName());
+    comp_hand_total = b.calculateHand(b.compHand);
+    System.out.println("New value is " + comp_hand_total);
+  }
+  else if (comp_hand_total > 17){
+    System.out.println("Dealer stays");
+  }
+
   }
 }
+//Need Scanner class for reader input (have done this)
+//if you choose option 1 then will need to loop again- Scanner will be inside of loop
+//indefinite loop- while loop until hit 2 or 3 or bust
+//if userHand busts then skip compHand
+//two while loops for userHand and compHand- compHand is outside of userHand loop
+//then resolve game
