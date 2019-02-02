58e411a334935da55ed8c68ef1506e615d23ed33
Eva Gleitz <eva_gleitz@m013393.local>
2019-02-01 17:50:33 -0800

    second try

--- MyFrame.java
+++ MyFrame.java
@@ -9,28 +9,31 @@ import javax.swing.JTextArea;
 
 class MyFrame extends JFrame {
   public MyFrame() {
-    setTitle("My Empty Frame");
+    setTitle("My Empty Frame"); //this sets up the window size
     setSize(300,200);
     setLocation(10,200);
-    Container c = getContentPane();
+
+    Container c = getContentPane(); //this allows stuff to be added to the window
     JButton button = new JButton ("Yeet It");
     c.add(button, BorderLayout.SOUTH);
-    addWindowListener(new WindowAdapter() {
+
+    addWindowListener(new WindowAdapter() { //this makes it so when the window is closed it terminates
   	  	public void windowClosing(WindowEvent e) {
   		   System.exit(0);
   	  	} //windowClosing
   	} );
-    JTextArea a = new JTextArea ();
+
+    JTextArea a = new JTextArea (); //this allows text to be in the center of the window
     c.add(a, BorderLayout.CENTER);
 
-    button.addActionListener(new ActionListener() {
+    button.addActionListener(new ActionListener() { //this tells the button what to do
       public void actionPerformed(ActionEvent e){
-				a.append("YEET ME OVER THE RAINBOW\nYEET ME REALLY FAR\n"); 
+				a.append("YEET ME OVER THE RAINBOW\nYEET ME REALLY FAR\n");
 			}
     });
 }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) { //makes the frame appear
     BorderLayout j = new BorderLayout();
     JFrame f = new MyFrame();
     f.show(); //shows actual window
