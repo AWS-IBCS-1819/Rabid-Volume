b75d147bb655f84929bf595c7e7b8ff5886a4a0b
Eva Gleitz <eva_gleitz@m013393.local>
2019-02-16 18:14:29 -0800

    Finished:D

--- FunWin.java
+++ FunWin.java
@@ -1,54 +1,62 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 
 public class FunWin extends Frame implements WindowListener,ActionListener {
         TextField text = new TextField(40);
         Button b;
         Button b2;
-        Checkbox za;
-        Checkbox sta;
-        Checkbox gna;
-        CheckboxGroup gg;
+        Button b3;
+        Button b4;
+        Button b5;
+        Button b6;
         private int numClicks = 0;
         private String choice = "";
         Gradebook g;
 
         public FunWin(String title) {
 
-          //create Gradebook
-        //  Gradebook g = new Gradebook("grades.txt");
 
                 super(title);
+                setLayout(new FlowLayout());
+                setBackground(Color.blue);
+                addWindowListener(this);
 
+                //Create Gradebook
                 g = new Gradebook("grades.txt");
 
-                setLayout(new FlowLayout());
-                setBackground(Color.cyan);
-                addWindowListener(this);
-                b = new Button("Thing 1");
+                b = new Button("Top Grade");
                 add(b);
-                b2 = new Button("Invisible?");
+
+                b2 = new Button("Low Grade");
                 add(b2);
+
+                b3= new Button("Mean Grade");
+                add(b3);
+
+                b4= new Button("Grade Spread");
+                add(b4);
+
+                b5 = new Button("Teacher");
+                add(b5);
+
+                b6 = new Button("Student Roster");
+                add(b6);
+
                 add(text);
-                gg = new CheckboxGroup();
-                za = new Checkbox("pizza", gg, false);
-                sta = new Checkbox("pasta", gg, false);
-                gna = new Checkbox("lasagna", gg, false);
-                add(za);
-                add(sta);
-                add(gna);
                 b.setBackground(Color.red);
                 Color c = new Color(2,7,80);
-                b2.setBackground(c);
-                b.setForeground(c);
-                b2.setForeground(Color.green);
+                b2.setBackground(Color.red);
+                b3.setBackground(Color.red);
+                b4.setBackground(Color.red);
+                b5.setBackground(Color.red);
+                b6.setBackground(Color.red);
                 b.addActionListener(this);
-                b2.addActionListener(new ActionListener() {
-                  public void actionPerformed(ActionEvent e) {
-                    numClicks--;
-                    text.setText("Button Clicked " + numClicks + " times");
-                  }
-                });
+                b2.addActionListener(this);
+                b3.addActionListener(this);
+                b4.addActionListener(this);
+                b5.addActionListener(this);
+                b6.addActionListener(this);
         }
 
         public static void main(String[] args) {
@@ -58,9 +66,58 @@ public class FunWin extends Frame implements WindowListener,ActionListener {
         }
 
         public void actionPerformed(ActionEvent e) {
-            text.setText(Double.toString(g.topGrade()));
-        }
+          Object src = e.getSource();
+            if (src == b){
+              text.setText(Double.toString(g.topGrade()));
+            }
+            if (src == b2){
+              text.setText(Double.toString(g.lowGrade()));
+            }
+            if (src == b3) {
+              text.setText(Double.toString(g.meanGrade()));
+            }
+           if(src == b4){
 
+            int[] grades = g.gradeSpread();
+          ;
+              String x = "";
+
+              String[] _V = new String[5];
+              _V[0] = "A";
+              _V[1] = "B";
+              _V[2] = "C";
+              _V[3] = "D";
+              _V[4] = "F";
+
+              for (int i=0; i < grades.length; i++) {
+                if (i == 0){
+                  x = "There is " + grades[i] +" "+ _V[i] +"'s ";
+                }
+                if(i == 1) {
+                  x += " "+grades[i] +" "+ _V[i] +"'s ";
+                }
+                if(i==2){
+                  x += " " + grades[i] +" "+ _V[i] +"'s ";
+                }
+                if(i==3){
+                  x += " " + grades[i] +" "+ _V[i] +"'s ";
+                }
+                if(i==4){
+                  x += " " + grades[i] +" "+ _V[i] +"'s ";
+                }
+              }
+              text.setText(x);
+
+
+            }
+            if (src == b5){
+              text.setText(g.getTeacher().toString());
+            }
+            if(src == b6){
+              text.setText(g.getStudents().toString());
+            }
+
+        }
 
         public void windowClosing(WindowEvent e) {
                 dispose();
