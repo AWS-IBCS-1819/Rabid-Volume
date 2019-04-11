05580ad197de2d8ab1f53b9f19deec98872ba380
Eva Gleitz <eva_gleitz@m013393.local>
2019-04-10 22:10:21 -0700

    Hash

--- HashTest.java
+++ HashTest.java
@@ -3,18 +3,29 @@ import java.io.*;
 
 public class HashTest {
 
-
-
-  public HashTest() {
-
-  }
-
   public static void main(String[] args) {
-    System.out.println("Input the code");
-    Scanner reader = new Scanner(System.in);
-    int n = 0;
-    n = reader.nextInt();
-    System.out.println("The code is:"+n);
-    String code = Integer.toString(n);
+
+    ArrayList<Integer> code = new ArrayList<Integer>();
+
+    code.add(6);
+    code.add(8);
+    code.add(3);
+
+    System.out.println("This is the start code: " + code);
+
+    int z = code.get(0);
+
+    if(z <= 5) {
+      int y = 0;
+      y = code.get(1) + code.get(2);
+      code.set(0, y);
+    }
+    if(z > 5) {
+      int u = 0;
+      u = code.get(1) * code.get(2);
+      code.set(0, u);
+    }
+
+    System.out.println("This is the new code: " + code);
   }
 }
